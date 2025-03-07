6e85bfa3b3085505abd419d877385969b932cebe
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-03-07 17:39:57 +0000

    Updated

--- area_per_circle.py
+++ area_per_circle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 # Created by: Gustav Ihlenfeld
-# Created on: March 4, 2025
+# Created on: March 5, 2025
 # This program calculates the circumference and area of a circle
 
 import math
 
 
 def main():
-    # Input
+    # Input (radius, fixed)
     radius = 12
-    # Process
+    # Process the area and circumference of a circle
     area = math.pi * (radius**2)
     circumference = 2 * math.pi * radius
 
-    # Output
+    # Output the results (area and circumference)
     print("\nThe area of the circle is {:.2f} square units.".format(area))
     print("The circumference of the circle is {:.2f} units.".format(circumference))
+
+    # Call function
 
 
 if __name__ == "__main__":
